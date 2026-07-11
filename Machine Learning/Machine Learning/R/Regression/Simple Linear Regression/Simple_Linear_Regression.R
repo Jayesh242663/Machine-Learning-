8ac6a0ee4c74bcf73ecf99c86c4411b7b7372510
Jayesh242663 <jayeshchanne9@gmail.com>
2026-07-12 02:34:53 +0530

    Stared with Data-Preprocessing and Regression(simple linear regression)

---
+++ Machine Learning/Machine Learning/R/Regression/Simple Linear Regression/Simple_Linear_Regression.R
@@ -0,0 +1,39 @@
+#Simple Linear Regression
+
+#Importing the dataset 
+dataset = read.csv('Salary_Data.csv')
+#dataset = dataset[, 2:3]
+
+#splitting the dataset into Training and Test set 
+#install.package('caTools')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+#training_set[, 2:3] = scale(training_set[, 2:3])
+#test_set[, 2:3] = scale(test_set[, 2:3])
+
+#Fitting Simple Linear Regression to the Training set 
+regressor = lm(formula= Salary ~ YearsExperience, 
+               data = training_set)
+
+#Predict the test set results 
+y_pred  = predict(regressor, newdata = test_set)
+print(y_pred)
+
+#Visualising the Training the set results
+#install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+              color = 'blue') +
+  
+  ggtitle('Salary vs Expreince(traing set)') +
+  xlab('Years of exopericnce')+
+  ylab('Salary')
